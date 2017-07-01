7556554cb967a28b11d823eb12a90a650f8361d8
nitikagoel01 <nitikagoel.1693@gmail.com>
2017-07-01 07:59:28 +0000

    Done

--- build.py
+++ build.py
@@ -2,32 +2,30 @@ import pandas as pd
 
 
 def load_data():
-    """
-        Enter your code here
-    """
-
+    df = pd.read_csv('files/olympics.csv',skiprows=1)
+    df.rename(columns={'01 !': 'Gold', '02 !': 'Silver','03 !':'Bronze'}, inplace=True)
+    country_names = [x.split('\xc2\xa0(')[0] for x in df.iloc[:,0]]
+    df.set_index(pd.Series(country_names), inplace=True)
+    df.rename(columns={"Unnamed: 0" : "Country"},inplace = True)
+    df.iloc[:,0] = country_names
+    df.drop(['Totals'],axis=0, inplace= True)
+    return df
 
 def first_country(df):
-    """
-        Enter your code here
-    """
-
+    return df.iloc[0,:]
 
 def gold_medal(df):
-    """
-        Enter your code here
-    """
-
+    return df['Gold'].argmax()
 
 def biggest_difference_in_gold_medal(df):
-    """
-        Enter your code here
-    """
+    comparison_data = df.iloc[:,[0,5,10]]
+    comparison_data['Difference'] = comparison_data.iloc[:,1] - comparison_data.iloc[:,2]
+    return comparison_data.iloc[:,3].idxmax()
+
 
 def get_points(df):
-    """
-    Enter your code here
-    """
+    df['Points'] = 3*df.iloc[:,12] + 2*df.iloc[:,13] + df.iloc[:,14]
+    return df.loc[:,'Points']
 
 
 # df = load_data()
